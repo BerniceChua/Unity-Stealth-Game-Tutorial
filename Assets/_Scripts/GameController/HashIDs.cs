97c31d9210b9c4bdf8dd9c47b05ea4fc3c645762
BerniceChua <Bernice.Chua.415@gmail.com>
2017-02-04 23:32:14 -0800

    Create HashIDs so it's easier to do animations later.

---
+++ Assets/_Scripts/GameController/HashIDs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashIDs : MonoBehaviour {
+    public int dyingState;
+    public int deadBool;
+    public int locomotionState;
+    public int shoutState;
+    public int speedFloat;
+    public int sneakingBool;
+    public int shoutingBool;
+    public int playerInSightBool;
+    public int shotFloat;
+    public int aimWeightFloat;
+    public int angularSpeedFloat;
+    public int openBool;
+
+    void Awake() {
+        dyingState = Animator.StringToHash("Base Layer.Dying");
+        deadBool = Animator.StringToHash("Dead");  // since we're not referencing a state and we're just referencing a parameter, we don't need the prefix here.
+        locomotionState = Animator.StringToHash("Base Layer.LocomotionBlendTree");
+        shoutState = Animator.StringToHash("Shouting.Shout");
+        speedFloat = Animator.StringToHash("Speed");
+        sneakingBool = Animator.StringToHash("Sneaking");
+        shoutingBool = Animator.StringToHash("Shouting");
+        playerInSightBool = Animator.StringToHash("PlayerInSight");
+        shotFloat = Animator.StringToHash("Shot");
+        aimWeightFloat = Animator.StringToHash("AimWeight");
+        angularSpeedFloat = Animator.StringToHash("AngularSpeed");
+        openBool = Animator.StringToHash("Open");
+    }
+}
