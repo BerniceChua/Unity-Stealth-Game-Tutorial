802625b78dcc0ded8cbf4662cef6a9bc39d0387a
BerniceChua <Bernice.Chua.415@gmail.com>
2017-02-08 22:09:21 -0800

    Add helper file AnimatorSetup.cs.

---
+++ Assets/_Scripts/Unapplied/AnimatorSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSetup {
+    // Damping will make the animations not so jerky when moving. That's why we have this helper class.
+
+    public float speedDampTime = 0.1f;
+    public float angularDampTime = 0.7f;
+    public float angleResponseTime = 0.6f;
+
+    private Animator anim;
+    private HashIDs hash;
+
+    public AnimatorSetup(Animator animator, HashIDs hashIDs) {
+        anim = animator;
+        hash = hashIDs;
+    }
+
+    public void Setup(float speed, float angle) {
+        float angularSpeed = angle / angleResponseTime;
+
+        anim.SetFloat(hash.speedFloat, speed, speedDampTime, Time.deltaTime);
+        anim.SetFloat(hash.angularSpeedFloat, angularSpeed, angularDampTime, Time.deltaTime);
+    }
+}
